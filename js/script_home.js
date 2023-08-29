7d8b815a09206f90a2943337f57b06e9193c17f6
rafaelpequino <o.rafaelpequino@gmail.com>
2023-08-29 18:23:27 -0300

    almost-end-home

--- js/script_home.js
+++ js/script_home.js
@@ -9,9 +9,24 @@ function formatNumberToShort(number) {
 //FORMATAR VALORES
 document.addEventListener('DOMContentLoaded', () => {
     const valuesAccounts = document.getElementsByClassName('account');
+    const valuesPayCard = document.getElementsByClassName('pay-card');
     
     Array.from(valuesAccounts).forEach(account => {
         const valueElement = account.querySelector('.value-account');
+        const originalValue = parseFloat(valueElement.textContent.replace(/,/g, ''));
+        
+        const formattedValueWithCurrency = originalValue.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+        
+        const formattedValueShort = formatNumberToShort(originalValue);
+        
+        valueElement.innerHTML = `<span>R$</span>${formattedValueWithCurrency.slice(3)}`;
+    });
+
+    Array.from(valuesPayCard).forEach(card => {
+        const valueElement = card.querySelector('.value-pay-card');
         const originalValue = parseFloat(valueElement.textContent.replace(/,/g, ''));
         
         const formattedValueWithCurrency = originalValue.toLocaleString('pt-BR', {
