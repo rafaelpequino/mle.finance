27b9b738f21441d54510fd902d1783116c9d7cc1
rafaelpequino <o.rafaelpequino@gmail.com>
2023-09-25 16:47:27 -0300

    continue-metas

--- js/script_transacoes.js
+++ js/script_transacoes.js
@@ -28,6 +28,38 @@ document.addEventListener('DOMContentLoaded', () => {
     const programadas = document.querySelectorAll('.programadas>div:last-child')
     for (let i = 0; i < programadas.length; i++) {
         formatValues(programadas[i]);
-}
+    }
+
+
+    //SHOW DISPLAY FILTER
+    const btnFilter = document.getElementById('btn-filter')
+    const displayFilter = document.getElementById('display-filter')
+    const blackout = document.getElementById('blackout')
+
+    btnFilter.addEventListener('click', openDisplayFilter)
+    document.addEventListener('keydown', function(event) {
+        if (event.keyCode === 27) {
+            if (displayFilter.getAttribute("data-status") === "opened") {
+                openDisplayFilter();
+            }
+        }
+    });
+    blackout.addEventListener('click', openDisplayFilter)
+
+    function openDisplayFilter() {
+        let displayStatus = displayFilter.getAttribute("data-status"); // Defina displayStatus dentro da função
+
+        if (displayStatus === "closed") {
+            displayFilter.style.transform = "scale(1)";
+            blackout.style.display = "block";
+            displayFilter.setAttribute("data-status", "opened");
+        } else {
+            displayFilter.style.transform = "scale(0)";
+            blackout.style.display = "none";
+            displayFilter.setAttribute("data-status", "closed");
+        }
+    }
+
+    
 
 })
