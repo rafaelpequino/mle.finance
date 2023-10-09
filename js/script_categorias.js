fec0f52c066a4f330d1003327b77ade629c2ecce
rafaelpequino <o.rafaelpequino@gmail.com>
2023-10-09 12:34:08 -0300

    categorias

---
+++ js/script_categorias.js
@@ -0,0 +1,28 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //FORMATAR VALORES
+    function formatValues (element) {
+        const originalValue = parseFloat(element.innerText)
+        const formattedValue = originalValue.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+        
+        if (originalValue >= 0) {
+            element.innerHTML = `<span>R$ </span>${formattedValue.slice(3)}`;
+        } else {
+            element.innerHTML = `<span>R$ -</span>${formattedValue.slice(4)}`;
+            element.style.color = "var(--expenses-color)"
+        }
+    }
+    //formatValues(document.getElementById('balance'))
+    
+    
+    const limitsCards = document.querySelectorAll('.limit-category>div>span')
+    for (let i = 0; i < limitsCards.length; i++) {
+        formatValues(limitsCards[i]);
+    }
+    
+    });
+    
