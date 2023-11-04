0d78a17dfe3ed6905e80b35ff06e77c7849a292a
rafaelpequino <o.rafaelpequino@gmail.com>
2023-11-04 20:36:12 -0300

    end_login

--- js/script_login-cadastro.js
+++ js/script_login-cadastro.js
@@ -1,27 +1,49 @@
+//MOVIMENTO DA LABEL E-MAIL
 const labelEmail = document.getElementById('label-email');
 const inputEmail = document.getElementById('input-email');
 
-inputEmail.addEventListener('focus', alterLabelEmail);
-inputEmail.addEventListener('blur', alterLabelEmail);
+inputEmail.addEventListener('focus', () => {
+    labelEmail.classList.remove('disabled-label');
+    labelEmail.classList.remove('filled-input');
+    labelEmail.classList.add('non-disabled-label');
+})
 
-inputEmail.addEventListener('input', function() {
-    if (inputEmail.value !== "") {
-        inputEmail.removeEventListener('focus', alterLabelEmail);
-    } else {
-        inputEmail.addEventListener('focus', alterLabelEmail);
-    }
-});
-
-function alterLabelEmail() {
-    labelEmail.classList.toggle('disabled-label');
-    labelEmail.classList.toggle('non-disabled-label');
-
+inputEmail.addEventListener('blur', () => {
     if (inputEmail.value !== "") {
         labelEmail.classList.remove('disabled-label');
         labelEmail.classList.remove('non-disabled-label');
         labelEmail.classList.add('filled-input');
     } else {
         labelEmail.classList.remove('filled-input');
+        labelEmail.classList.remove('non-disabled-label');
+        labelEmail.classList.add('disabled-label');
 
     }
-}
+});
+
+
+//MOVIMENTO DA LABEL SENHA
+const labelSenha = document.getElementById('label-senha');
+const inputSenha = document.getElementById('input-senha');
+
+inputSenha.addEventListener('focus', () => {
+    labelSenha.classList.remove('disabled-label');
+    labelSenha.classList.remove('filled-input');
+    labelSenha.classList.add('non-disabled-label');
+})
+
+inputSenha.addEventListener('blur', () => {
+    if (inputSenha.value !== "") {
+        labelSenha.classList.remove('disabled-label');
+        labelSenha.classList.remove('non-disabled-label');
+        labelSenha.classList.add('filled-input');
+    } else {
+        labelSenha.classList.remove('filled-input');
+        labelSenha.classList.remove('non-disabled-label');
+        labelSenha.classList.add('disabled-label');
+
+    }
+});
+
+
+
