b92416717cf16914c3b591269d0e08525dec1a7e
rafaelpequino <o.rafaelpequino@gmail.com>
2023-11-06 09:53:10 -0300

    label-input-config

--- js/script_config.js
+++ js/script_config.js
@@ -1,4 +1,31 @@
-//MÁSCARA
-$(document).ready(function(){
-    $('#cellphone').mask('(00) 00000-0000');
+//EFEITO DOS INPUTS E LABELS
+const inputContainers = document.querySelectorAll('.input-container')
+
+inputContainers.forEach(inputContainer => {
+    let label = inputContainer.querySelector('label')
+    let input = inputContainer.querySelector('input')
+
+    input.addEventListener('focus', () => {
+        label.classList.remove('disabled-label', 'filled-input')
+        label.classList.add('non-disabled-label')
+    });
+
+    input.addEventListener('blur', () => {
+        if (input.value !== "") {
+            label.classList.remove('disabled-label', 'non-disabled-label')
+            label.classList.add('filled-input')
+        } else {
+            label.classList.remove('filled-input', 'non-disabled-label')
+            label.classList.add('disabled-label')
+        }
+    })
 })
+
+
+//MÁSCARA DE TELEFONE
+if (document.getElementById('input-cellphone')) {
+    $(document).ready(function(){
+        $('#input-cellphone').mask('(00) 00000-0000');
+    })
+    
+}
