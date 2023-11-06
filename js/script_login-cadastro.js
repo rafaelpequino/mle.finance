dc9581fbbb09d11143f4519f07a20bcd94b5a632
rafaelpequino <o.rafaelpequino@gmail.com>
2023-11-06 09:47:19 -0300

    login-cadastro

--- js/script_login-cadastro.js
+++ js/script_login-cadastro.js
@@ -1,49 +1,31 @@
-//MOVIMENTO DA LABEL E-MAIL
-const labelEmail = document.getElementById('label-email');
-const inputEmail = document.getElementById('input-email');
+//EFEITO DOS INPUTS E LABELS
+const inputContainers = document.querySelectorAll('.input-container')
 
-inputEmail.addEventListener('focus', () => {
-    labelEmail.classList.remove('disabled-label');
-    labelEmail.classList.remove('filled-input');
-    labelEmail.classList.add('non-disabled-label');
+inputContainers.forEach(inputContainer => {
+    let label = inputContainer.querySelector('label')
+    let input = inputContainer.querySelector('input')
+
+    input.addEventListener('focus', () => {
+        label.classList.remove('disabled-label', 'filled-input')
+        label.classList.add('non-disabled-label')
+    });
+
+    input.addEventListener('blur', () => {
+        if (input.value !== "") {
+            label.classList.remove('disabled-label', 'non-disabled-label')
+            label.classList.add('filled-input')
+        } else {
+            label.classList.remove('filled-input', 'non-disabled-label')
+            label.classList.add('disabled-label')
+        }
+    })
 })
 
-inputEmail.addEventListener('blur', () => {
-    if (inputEmail.value !== "") {
-        labelEmail.classList.remove('disabled-label');
-        labelEmail.classList.remove('non-disabled-label');
-        labelEmail.classList.add('filled-input');
-    } else {
-        labelEmail.classList.remove('filled-input');
-        labelEmail.classList.remove('non-disabled-label');
-        labelEmail.classList.add('disabled-label');
 
-    }
-});
-
-
-//MOVIMENTO DA LABEL SENHA
-const labelSenha = document.getElementById('label-senha');
-const inputSenha = document.getElementById('input-senha');
-
-inputSenha.addEventListener('focus', () => {
-    labelSenha.classList.remove('disabled-label');
-    labelSenha.classList.remove('filled-input');
-    labelSenha.classList.add('non-disabled-label');
-})
-
-inputSenha.addEventListener('blur', () => {
-    if (inputSenha.value !== "") {
-        labelSenha.classList.remove('disabled-label');
-        labelSenha.classList.remove('non-disabled-label');
-        labelSenha.classList.add('filled-input');
-    } else {
-        labelSenha.classList.remove('filled-input');
-        labelSenha.classList.remove('non-disabled-label');
-        labelSenha.classList.add('disabled-label');
-
-    }
-});
-
-
-
+//MÁSCARA DE TELEFONE
+if (document.getElementById('input-cellphone')) {
+    $(document).ready(function(){
+        $('#input-cellphone').mask('(00) 00000-0000');
+    })
+    
+}
